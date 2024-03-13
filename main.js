4e307fc360b9c9c45fdd18ec165d69e534672c81
sherzod <sherzodibodov098@gmail.com>
2024-03-13 10:51:30 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let num1 = prompt("son kiriting");
+let calc = prompt("amalni kiriting");
+let num2 = prompt("son kiriting");
+
+if (calc == "+") {
+  console.log(+num1 + +num2);
+} else if (calc == "-") {
+  console.log(num1 - num2);
+} else if (calc == "*") {
+  console.log(num1 * num2);
+} else if (calc == "/") {
+  console.log(num1 / num2);
+} else if (calc == "%") {
+  console.log(num1 % num2);
+}
